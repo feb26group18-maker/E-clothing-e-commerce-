8368a944b988491d09eaae16de0a884eb98d43bc
saloni <salonigulatkar04@gmail.com>
2026-07-31 17:40:14 +0530

    Restructuring the project

--- backend/UserService/UserService/src/main/java/com/example/demo/repository/SellerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Seller;
-
-public interface SellerRepository extends JpaRepository<Seller, Integer> {
-
-}
